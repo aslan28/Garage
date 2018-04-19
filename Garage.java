a78881b38a451f2655de6853666160b9d3a1db28
aslan28 <38540070+aslan28@users.noreply.github.com>
2018-04-19 23:20:55 +0100

    Update Garage.java

--- Garage.java
+++ Garage.java
@@ -1,41 +1,48 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 class Garage {
 
 	public static ArrayList<Vehicle> vehicleList = new ArrayList<>();
 
-	private void addVehicle(Vehicle v) {
-		vehicleList.add(v);
+	private static void addVehicle(Vehicle v)
+	{
+		vehicleList.add(v);	
 	}
-
+	
+	
 	private void removeVehicle(Vehicle v)
 	{
 		Iterator i = vehicleList.iterator();
 		while(i.hasNext())
 		{
-			Vehicle veh = iterator.next();
-			if(veh.equals(v)
+			Vehicle veh = (Vehicle) i.next();
+			if(veh.equals(v)) ;
 			   {
-				   iterator.remove();
+				   i.remove();
 			   }
 		}
 	}
-
-	private void removeVehicle(int id) {
+			   
+	private void removeVehicle(int id)
+	{
 		Iterator i = vehicleList.iterator();
-		while (i.hasNext()) {
-			Vehicle veh = iterator.next();
-			if (veh.getID() == id) {
-				iterator.remove();
-			}
+		while(i.hasNext())
+		{
+			Vehicle veh = (Vehicle) i.next();
+			if(veh.getID()== id)
+			   {
+				   i.remove();
+			   }
 		}
 	}
-
-	private void emptyList() {
+	
+	private void emptyList()
+	{
 		vehicleList.clear();
 	}
 
-	private float generateBill(Vehicle v) {
+	/*private float generateBill(Vehicle v) {
 		float total = 0.00f;
 
 		if (v.isCar)
@@ -61,21 +68,23 @@ class Garage {
 			value = ((300 - 60) * random.nextFloat() + 60);
 		}
 		return value;
-	}
+	}*/
 
 	public static void main(String[] args) {
-		Car c1 = new Car();
-		Car c2 = new Car("BMW", "116i", 2006);
-		Van v1 = new Van("", "", 2006);
-		Van v2 = new Van("", "", 2006);
-		Motorcycle m1 = new Motorcycle("", "", 2006);
-		Motorcycle m2 = new Motorcycle("", "", 2006);
+		Car c1 = new Car(1, "Ford", true, false, false, "Manual", 5);
+		/*Car c2 = new Car(2, "BMW", true, false, false, "Manual", 5);
+		Van v1 = new Van(3, "Fiat", false, false, true, "Fiat", 3);
+		Van v2 = new Van(4, "Citreon", false, false, true, "Citreon", 4);
+		Motorcycle m1 = new Motorcycle(5, "BMW", false, true, false, "bmw", 5);
+		Motorcycle m2 = new Motorcycle(6, "Kawasaki", false, true, false, "kawasaki", 5);
 
 		System.out.println(c1);
 		System.out.println(c2);
 		System.out.println(v1);
 		System.out.println(v2);
-		System.out.println(m1);
-		System.out.println(m2);
+		System.out.println(m1);*/
+		addVehicle(c1);
+		
+		System.out.println(vehicleList);
 	}// end of main method
 }
