09d6de2ff0c6eb1cbcfc53750168882ad66ac348
aslan28 <38540070+aslan28@users.noreply.github.com>
2018-04-22 00:25:01 +0100

    Update Garage.java

--- Garage.java
+++ Garage.java
@@ -25,10 +25,6 @@ class Garage {
 		}
 	}
 
-	//private static void emptyList() {
-		//vehicleList.clear();
-	//}
-
 	private static float generateBill(Vehicle v) {
 		float total = 0.00f;
 
@@ -60,26 +56,20 @@ class Garage {
 	}
 
 	public static void main(String[] args) {
-		Car c1 = new Car(1, "Ford", true, false, false, "Manual", 5);
-		Car c2 = new Car(2, "BMW-1Series", true, false, false, "Manual", 5);
+		Car c1 = new Car(1, "BMW 1Series", true, false, false, "Manual", 5);
 		Van v1 = new Van(3, "Fiat", "Doblo", false, false, true);
-		Van v2 = new Van(4, "Mercedes-Benz", "Sprinter", false, false, true);
-		Motorcycle m1 = new Motorcycle(5, "BMW", 600, "Petrol", false, true, false);
-		Motorcycle m2 = new Motorcycle(6, "Kawasaki", 600, "Petrol", false, true, false);
+		Motorcycle m1 = new Motorcycle(5, "Yamaha", 600, "Petrol", false, true, false);
 
 		addVehicle(c1);
-		addVehicle(c2);
 		addVehicle(v1);
-		addVehicle(v2);
 		addVehicle(m1);
-		addVehicle(m2);
 
 		System.out.println("Current vehicles in the garage:\n" + vehicleList + "\n\nBill Calculation (VAT inclusive):");
 
-		System.out.println("Total Bill for BMW-1Series: " + generateBill(c2));
-		System.out.println("Total Bill for Mercedez-Benz: " + generateBill(v2));
-		System.out.println("Total Bill for Kawasaki: " + generateBill(m2) + "\n\n");
+		System.out.println("Total Bill for BMW-1Series: " + generateBill(c1));
+		System.out.println("Total Bill for Mercedez-Benz: " + generateBill(v1));
+		System.out.println("Total Bill for Kawasaki: " + generateBill(m1) + "\n\n");
 		
-		System.out.println("Total vehicles now present in the garage" + vehicleList);
+		System.out.println("Total vehicles now present in the garage: " + vehicleList);
 	}// end of main method
 }
