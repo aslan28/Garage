9416e28ceec2c53a08be897a4b03d24dded13a79
aslan28 <38540070+aslan28@users.noreply.github.com>
2018-04-22 00:10:03 +0100

    Update Motorcycle.java

--- Motorcycle.java
+++ Motorcycle.java
@@ -3,16 +3,15 @@ public class Motorcycle extends Vehicle {
 	// private variables to store the attributes of a motorcycle
 	private String fuelType = "";
 	private int cc;
-	//private int year;
+	// private int year;
 
-	public Motorcycle(int id, String manufacturer, int cc, String fuelType, boolean isCar, boolean isMotorcycle, boolean isVan)
-	{
-		super(id, manufacturer, isCar, isMotorcycle, isVan);
-		
+	public Motorcycle(int id, String manufacturer, int cc, String fuelType, boolean isCar, boolean isMotorcycle,
+			boolean isVan) {
+		super(id, manufacturer, false, true, false);
+
 		this.fuelType = fuelType;
 		this.cc = cc;
 	} // end of constructor
-
 
 	public String getFuelType() {
 		return fuelType;
@@ -21,15 +20,4 @@ public class Motorcycle extends Vehicle {
 	public double getCc() {
 		return cc;
 	}
-
-	
-	/**
-	 * This method returns a String to represent a car
-	 * 
-	 * @return
-	 *
-	*public String toString() {
-	*	return (make + " " + model + " " + year);
-	*}
-	*/
 }
