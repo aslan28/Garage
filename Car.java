5faefda5be01c531a8182d0bbd7c3829a810a9c9
aslan28 <38540070+aslan28@users.noreply.github.com>
2018-04-22 00:09:31 +0100

    Update Car.java

--- Car.java
+++ Car.java
@@ -4,28 +4,19 @@ public class Car extends Vehicle {
 	private int seats;
 	private String gearType;
 
-	public Car(int id, String manufacturer, boolean isCar, boolean isMotorcycle, boolean isVan, String gearType, int seats) {
-		super(id, manufacturer, isCar, false, false);
+	public Car(int id, String manufacturer, boolean isCar, boolean isMotorcycle, boolean isVan, String gearType,
+			int seats) {
+		super(id, manufacturer, true, false, false);
 
 		this.seats = seats;
 		this.gearType = gearType;
 	} // end of constructor
 
-	
 	public int getSeats() {
 		return seats;
 	}
-	
+
 	public String getGearType() {
 		return gearType;
 	}
-	/**
-	 * This method returns a String to represent a car
-	 * 
-	 * @return
-	 *
-	*public String toString() {
-	*	return (make + " " + model + " " + year);
-	*}
-	*/
 }
