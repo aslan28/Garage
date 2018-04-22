36696debc02dc83bedd8175bfefb2cdf421e2e35
aslan28 <38540070+aslan28@users.noreply.github.com>
2018-04-22 23:52:32 +0100

    Update Van.java

--- Van.java
+++ Van.java
@@ -4,8 +4,9 @@ public class Van extends Vehicle {
 	private String variant = "";
 
 	public Van(int id, String manufacturer, String variant, boolean isCar, boolean isMotorcycle, boolean isVan) {
-		super(id, manufacturer, false, false, true);
-		this.variant = variant;
+		super(id, manufacturer, false, false, true); // importing the characteristics of vehicle to car
+
+		this.variant = variant; // variable model variant for Van
 
 	} // end of constructor
 
